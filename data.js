44058ad8a519ae9d5b2502a8ca78e1ee3f91c45d
Toni Haryanto <toha.samba@gmail.com>
2022-04-09 11:03:54 +0700

    Initial commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_1925",
+      "name": "IMG_1925",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.08445141541908363,
+        "pitch": 0.1934606003404742,
+        "fov": 1.432757713412631
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.13573281697615514,
+          "pitch": 0.17808659818569694,
+          "rotation": 0,
+          "target": "1-img_1924"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_1924",
+      "name": "IMG_1924",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4181154555928916,
+          "pitch": 0.0284789843030282,
+          "rotation": 0,
+          "target": "0-img_1925"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
